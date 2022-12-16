6a21d7b79f54b3e9d89eac82d02da75b4adde247
HeoJiye <heoze1031@gmail.com>
2022-12-16 10:31:28 +0900

    refactor: 플레이어가 이동할 칸 입력, 게임 재시작/종료 여부 입력 입력값 기댓값과 비교 함수 분리 입력 기댓값 상수 선언

--- src/InputView.js
+++ src/InputView.js
@@ -6,7 +6,13 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const InputView = {
   MINIMUM_BRIDGE_SIZE: 3,
   MAXIMUM_BRIDGE_SIZE: 20,
+  
+  INPUT_UP_MOVING: 'U',
+  INPUT_DOWN_MOVING: 'D',
 
+  INPUT_RESTART: 'R',
+  INPUT_QUIT: 'Q',
+  
   /**
    * 다리의 길이를 입력받는다.
    */
@@ -34,9 +40,7 @@ const InputView = {
   readMoving() {
     MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (answer) => {
       try {
-        if(answer != 'U' && answer != 'D') {
-          throw '[ERROR] 입력 값이 기댓값과 다릅니다.';
-        }
+        this.isExpectValue(answer, [this.INPUT_UP_MOVING, this.INPUT_DOWN_MOVING]);
       } catch (e) {
         MissionUtils.Console.print(e);
       }
@@ -49,14 +53,18 @@ const InputView = {
   readGameCommand() {
     MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (answer) => {
       try {
-        if(answer != 'R' && answer != 'Q') {
-          throw '[ERROR] 입력 값이 기댓값과 다릅니다.';
-        }
+        this.isExpectValue(answer, [this.INPUT_RESTART, this.INPUT_QUIT]);
       } catch (e) {
         MissionUtils.Console.print(e);
       }
     });
   },
+
+  isExpectValue(value, expectedValues) {
+    if(expectedValues.indexOf(value) === -1) {
+      throw '[ERROR] 입력 값이 기댓값과 다릅니다.';
+    }
+  } 
 };
 
 module.exports = InputView;
