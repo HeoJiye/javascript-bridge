39b0555464c6c3ed73369efbfcf44c84cd31a76d
HeoJiye <heoze1031@gmail.com>
2022-12-16 10:25:02 +0900

    feat: 플레이어가 이동할 칸 입력, 게임 재시작/종료 여부 입력
    기능 단순 구현
    예외 처리) 입력 값이 범위에서 벗어난 경우

--- src/InputView.js
+++ src/InputView.js
@@ -31,12 +31,32 @@ const InputView = {
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    MissionUtils.Console.readLine('이동할 칸을 선택해주세요. (위: U, 아래: D)', (answer) => {
+      try {
+        if(answer != 'U' && answer != 'D') {
+          throw '[ERROR] 입력 값이 기댓값과 다릅니다.';
+        }
+      } catch (e) {
+        MissionUtils.Console.print(e);
+      }
+    });
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+  readGameCommand() {
+    MissionUtils.Console.readLine('게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)', (answer) => {
+      try {
+        if(answer != 'R' && answer != 'Q') {
+          throw '[ERROR] 입력 값이 기댓값과 다릅니다.';
+        }
+      } catch (e) {
+        MissionUtils.Console.print(e);
+      }
+    });
+  },
 };
 
 module.exports = InputView;
