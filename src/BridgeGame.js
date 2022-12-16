9cdfd651f553bd2d4b1135756e5ac0071eda8b4f
HeoJiye <heoze1031@gmail.com>
2022-12-16 12:59:55 +0900

    feat: 다리 건너기
    기능 단순 구현

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -2,12 +2,24 @@
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
+  #bridge;
+  #aheadBridge;
+
+  constructor(bridge) {
+    this.#bridge = this.#aheadBridge = bridge;
+  }
+
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(moving) {
+    if(moving != this.#aheadBridge.shift()) throw '다리 건너기 실패!';
+    
+    if(this.#aheadBridge.length == 0) return false;
+    return true;
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
