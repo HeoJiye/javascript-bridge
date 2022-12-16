4771b1599cc7cd4564ea0ecd06c6d0f9602be528
HeoJiye <heoze1031@gmail.com>
2022-12-16 12:59:11 +0900

    test: 다리 건너기
    BridgeGame 단위 테스트 추가
    다리 건너기 기능 테스트 추가

---
+++ __tests__/unit/BridgeGame.test.js
@@ -0,0 +1,27 @@
+const BridgeGame = require("../../src/BridgeGame");
+
+describe("BridgeGame unit test", () => {
+    test("다리 건너기", () => {
+        const bridgeGame = new BridgeGame(['U', 'D', 'U']);
+        
+        expect(bridgeGame.move('U')).toEqual(true);
+    });
+    test("다리 건너기: 예외) 건널 수 없는 경우 (1)", () => {
+        const bridgeGame = new BridgeGame(['U', 'D', 'U']);
+        
+        expect(() => bridgeGame.move('D')).toThrow();
+    });
+    test("다리 건너기: 예외) 건널 수 없는 경우 (1)", () => {
+        const bridgeGame = new BridgeGame(['U', 'D', 'U']);
+
+        bridgeGame.move('U');
+        expect(() => bridgeGame.move('U')).toThrow();
+    });
+    test("다리 건너기: 게임 성공", () => {
+        const bridgeGame = new BridgeGame(['U', 'D', 'U']);
+        
+        bridgeGame.move('U');
+        bridgeGame.move('D');
+        expect(bridgeGame.move('U')).toEqual(false);
+    });
+});
